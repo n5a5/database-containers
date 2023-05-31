16f8d04867ccbdf66fd01e92f301ebaa2e29b183
Nikita Akilov <26031301+n5a5@users.noreply.github.com>
2023-05-31 16:26:39 +0200

    set auth mode to none

--- neo4j.sh
+++ neo4j.sh
@@ -7,5 +7,6 @@ docker run -d \
     --name neo4j \
     -p 7474:7474 \
     -p 7687:7687 \
+    --env=NEO4J_AUTH=none \
     --mount type=volume,source=${VOLUME_NAME},target=/data \
     registry.hub.docker.com/library/neo4j
